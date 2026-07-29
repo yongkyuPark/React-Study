acbb8e15739333240f5de9b1e9e1b4051aae3202
Yongkyu Park <dydrbdjdsla@gmail.com>
2026-07-29 20:06:53 +0900

    feat : 자료형

---
+++ onebite-react/section01/chapter05.js
@@ -0,0 +1,28 @@
+// 1. Number Type
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+
+let inf = Infinity;
+let mInf = -Infinity;
+
+let nan = NaN;
+
+// 2. String Type
+let myName = "park";
+let myLocation = "서울";
+let introduce = myName + myLocation;
+
+// 템플릿 리터럴 문법
+let introduceText = `${myName}은 ${myLocation}에 거주합니다`;
+
+// 3. Boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+// 4. Null Type (아무것도 없다)
+let empty = null;
+
+// 5. Undefined Type
+let none;
+console.log(none);
