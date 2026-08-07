788aebb3f60cf26f12fb19e2652d897f90790225
Yongkyu Park <dydrbdjdsla@gmail.com>
2026-08-07 20:53:47 +0900

    feat : 단락 평가

---
+++ onebite-react/section02/chapter02.js
@@ -0,0 +1,20 @@
+// function returnFalse() {
+//   console.log("False 함수");
+//   return false;
+// }
+
+// function returnTrue() {
+//   console.log("True 함수");
+//   return true;
+// }
+
+// console.log(returnFalse() && returnTrue());
+
+// 단락 평가 활용 사례
+function printName(person) {
+  const name = person && person.name;
+  console.log(name || "person의 값이 없음");
+}
+
+printName();
+printName({ name: "park" });
