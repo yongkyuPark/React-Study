12fab9792ede5eee95392e324bca8b4780ad2943
Yongkyu Park <dydrbdjdsla@gmail.com>
2026-08-25 21:33:44 +0900

    feat : 배열 메서드 - 순회와 탐색

---
+++ onebite-react/section02/chapter08.js
@@ -0,0 +1,44 @@
+// 5가지 요소 순회 및 탐색 메서드
+// 1. forEach
+// 모든 요소를 순회하면서, 각각의 요소에 특정 동작을 수행시키는 메서드
+let arr1 = [1, 2, 3];
+
+arr1.forEach(function (item, idx, arr) {
+  //   console.log(idx, item * 2);
+});
+
+let doubledArr = [];
+
+arr1.forEach((item) => {
+  doubledArr.push(item * 2);
+});
+
+// 2. includes
+// 배열에 특정 요소가 있는지 확인하는 그런 메서드
+let arr2 = [1, 2, 3];
+let isIncludes = arr2.includes(10);
+
+// 3. indexOf
+// 특정 요소의 인덱스(위치)를 찾아서 반환하는 메서드
+let arr3 = [1, 2, 3];
+let index = arr3.indexOf(2);
+
+// let objectArr = [{ name: "park" }, { name: "kim" }];
+
+// console.log(objectArr.indexOf({ name: "park" }));
+
+// console.log(objectArr.findIndex((item) => item.name === "park"));
+
+// 4. findIndex
+// 모든 요소를 순회하면서, 콜백함수를 만족하는 그런
+// 특정 요소의 인덱스(위치)를 반환하는 메서드
+let arr4 = [1, 2, 3];
+const findedIndex = arr4.findIndex((item) => item === 2);
+
+// 5. find
+// 모든 요소를 순회하면서 콜백함수를 만족하는 요소를 찾는데, 요소를 그대로 반환
+
+let arr5 = [{ name: "park" }, { name: "kim" }];
+
+const finded = arr5.find((item) => item.name === "park");
+console.log(finded);
