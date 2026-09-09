357c19f06c08cf50e42b5b52d78af193f1b97f01
yongkyu <dydrbdjdsla@gmail.com>
2026-09-09 10:48:37 +0900

    feat: React 컴포넌트

--- onebite-react/section05/src/App.jsx
+++ onebite-react/section05/src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <h1>안녕 리액트!</h1>
+      <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
