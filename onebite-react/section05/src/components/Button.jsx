d343f6c506b04e8d7d2bd51392d621bd4119cd7d
yongkyu <dydrbdjdsla@gmail.com>
2026-09-10 09:58:46 +0900

    feat: 이벤트 처리하기

--- onebite-react/section05/src/components/Button.jsx
+++ onebite-react/section05/src/components/Button.jsx
@@ -1,6 +1,15 @@
 const Button = ({ children, text, color = "black" }) => {
+  const onClickButton = (e) => {
+    console.log(e);
+    console.log(text);
+  };
+
   return (
-    <button style={{ color: color }}>
+    <button
+      onClick={onClickButton}
+      //   onMouseEnter={onClickButton}
+      style={{ color: color }}
+    >
       {text} - {color.toUpperCase()}
       {children}
     </button>
