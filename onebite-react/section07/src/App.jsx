e927c736b37ef8378bddef9a618f627a709530eb
yongkyu <dydrbdjdsla@gmail.com>
2026-09-11 14:41:52 +0900

    feat: useEffect 사용하기

---
+++ onebite-react/section07/src/App.jsx
@@ -0,0 +1,33 @@
+import "./App.css";
+import Viewer from "./components/Viewer";
+import Controller from "./components/Controller";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log(`count: ${count}`);
+  }, [count]);
+  // 의존성 배열
+  // dependecy array
+  // deps
+
+  const onClickButton = (value) => {
+    setCount(count + value);
+  };
+
+  return (
+    <div className="App">
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer count={count} />
+      </section>
+      <section>
+        <Controller onClickButton={onClickButton} />
+      </section>
+    </div>
+  );
+}
+
+export default App;
